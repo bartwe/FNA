86ee251e0f65e9d44f1bb1b942a2048756f7b505
DeamonHunter <deamonhunterextra@outlook.com>
2021-08-13 14:29:13 +1000

    Add clipboard support to FNA.

--- src/Input/TextInputEXT.cs
+++ src/Input/TextInputEXT.cs
@@ -57,6 +57,30 @@ namespace Microsoft.Xna.Framework.Input
 			FNAPlatform.SetTextInputRectangle(rectangle);
 		}
 
+		/// <summary>
+		/// Checks if there is text in the clipboard.
+		/// </summary>
+		/// <returns>Clipboard has text.</returns>
+		public static bool HasClipBoardText() {
+			return FNAPlatform.HasClipboardText();
+		}
+
+		/// <summary>
+		/// Gets the current text in the clipboard.
+		/// </summary>
+		/// <returns>Clipboard Text.</returns>
+		public static string GetClipBoardText() {
+			return FNAPlatform.GetClipboardText();
+		}
+
+		/// <summary>
+		/// Sets the current text in the clipboard.
+		/// </summary>
+		/// <param name="text">The text to be set in the clipboard.</param>
+		public static void SetClipBoardText(string text) {
+			FNAPlatform.SetClipboardText(text);
+		}
+
 		#endregion
 
 		#region Internal Event Access Method
