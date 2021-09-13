0783c1ef7ce3346f75e3f487b5d610f4f97037d3
DeamonHunter <deamonhunterextra@outlook.com>
2021-09-13 14:21:02 +1000

    Don't repeat control characters every frame, instead give them a slight delay. (Still doesn't match text input but its closer)

--- src/FNAPlatform/SDL2_FNAPlatform.cs
+++ src/FNAPlatform/SDL2_FNAPlatform.cs
@@ -1110,6 +1110,7 @@ namespace Microsoft.Xna.Framework
 			{
 				if (textInputControlDown[i] && textInputControlRepeat[i] <= Environment.TickCount)
 				{
+					textInputControlRepeat[i] = Environment.TickCount + 40;
 					TextInputEXT.OnTextInputExt(FNAPlatform.TextInputKeys[i]);
 					TextInputEXT.OnTextInput(FNAPlatform.TextInputCharacters[i]);
 				}
